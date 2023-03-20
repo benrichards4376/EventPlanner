c7b2b3e55cd0041dd67d78794ac8860be9970734
BrooklynLovez <103601428+BrooklynLovez@users.noreply.github.com>
2023-03-20 17:54:51 -0400

    Create code2.js

---
+++ js/code2.js
@@ -0,0 +1,191 @@
+const urlBase = 'http://first-web.xyz/API';
+const extension = 'php';
+
+let userId = 0;
+let firstName = "";
+let lastName = "";
+
+function doLogin()
+{
+	userId = 0;
+	firstName = "";
+	lastName = "";
+
+	// get the incoming values
+	let login = document.getElementById("loginName").value;
+	let password = document.getElementById("loginPassword").value;
+
+	// get results from login
+	document.getElementById("loginResult").innerHTML = "";
+
+	// set values as a JSON string
+	let tmp = {login:login,password:password};
+	let jsonPayload = JSON.stringify( tmp );
+
+	let url = urlBase + '/Login.' + extension;
+
+	let xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.onreadystatechange = function()
+		{
+			// check state of API
+			if (this.readyState == 4 && this.status == 200)
+			{
+				let jsonObject = JSON.parse(xhr.responseText);
+				userId = jsonObject.user_id;
+				if( userId < 1 )
+				{
+					document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
+					return;
+				}
+
+				firstName = jsonObject.first_name;
+				lastName = jsonObject.last_name;
+
+				saveCookie();
+
+				window.location.href = "color.html";
+			}
+		}; // end onreadystatechange
+
+		// send everything to server
+		xhr.send(jsonPayload);
+	} // end try
+	catch(err)
+	{
+		document.getElementById("loginResult").innerHTML = err.message;
+	} // end catch
+
+} // end function doLogin
+
+function doRegister(){
+
+	// get the incoming values
+	let firstName = document.getElementById("firstName").value;
+	let lastName = document.getElementById("lastName").value;
+	let login = document.getElementById("userName").value;
+	let password = document.getElementById("password").value;
+
+	// get results from register & check for special characters
+	if(hasSpecialChar(login)){
+		document.getElementById("registerResult").innerHTML = "ERROR: YOU CAN'T HAVE SPECIAL CHARACTERS IN YOUR USERNAME";
+		return;
+	}
+	else if(hasSpecialChar(firstName)){
+		document.getElementById("registerResult").innerHTML = "ERROR: YOU CAN'T HAVE SPECIAL CHARACTERS IN YOUR FIRST NAME";
+		return;
+	}
+	else if(hasSpecialChar(lastName)){
+		document.getElementById("registerResult").innerHTML = "ERROR: YOU CAN'T HAVE SPECIAL CHARACTER IN YOUR LAST NAME";
+		return;
+	}
+	else{
+		document.getElementById("registerResult").innerHTML = "";
+	}
+
+	// set the temp variables
+	let tmp = {
+        FirstName: firstName,
+        LastName: lastName,
+        Login: login,
+        Password: password
+    };
+
+	// set values as a JSON string
+	let jsonPayload = JSON.stringify(tmp);
+    let url = urlBase + '/Register.' + extension;
+
+	let xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+
+	try {
+        xhr.onreadystatechange = function ()
+		{
+			// check state of API
+            if (this.readyState != 4)
+			{
+                return;
+            }
+
+            if (this.status == 409)
+			{
+                return;
+            }
+
+			// if no error retrieve values
+            if (this.status == 200)
+			{
+				document.getElementById("firstName").value = "";
+				document.getElementById("lastName").value = "";
+				document.getElementById("register-username").value = "";
+				document.getElementById("register-password").value = "";
+
+				saveCookie();
+				moveTologin();
+            }
+        }; // end onreadystatechange
+
+		// send everything to server
+        xhr.send(jsonPayload);
+		
+    } // end try
+	catch (err)
+	{
+        document.getElementById("registerResult").innerHTML = err.message;
+    } // end catch
+
+} // end of doRegister function
+
+function saveCookie()
+{
+	let minutes = 20;
+	let date = new Date();
+	date.setTime(date.getTime()+(minutes*60*1000));
+	document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userId=" + userId + ";expires=" + date.toGMTString();
+} // end of saveCookie function
+
+function readCookie()
+{
+	userId = -1;
+	let data = document.cookie;
+	let splits = data.split(",");
+	for(var i = 0; i < splits.length; i++)
+	{
+		let thisOne = splits[i].trim();
+		let tokens = thisOne.split("=");
+		if( tokens[0] == "firstName" )
+		{
+			firstName = tokens[1];
+		}
+		else if( tokens[0] == "lastName" )
+		{
+			lastName = tokens[1];
+		}
+		else if( tokens[0] == "userId" )
+		{
+			userId = parseInt( tokens[1].trim() );
+		}
+	} // end for loop
+
+	if( userId < 0 )
+	{
+		window.location.href = "index.html";
+	}
+	else
+	{
+		document.getElementById("userName").innerHTML = "Logged in as " + firstName + " " + lastName;
+	}
+} // end function readCookie
+
+function doLogout()
+{
+	userId = 0;
+	firstName = "";
+	lastName = "";
+	document.cookie = "firstName= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
+	window.location.href = "index.html";
+} // end function doLogout
